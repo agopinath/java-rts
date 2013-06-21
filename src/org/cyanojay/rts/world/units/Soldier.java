5d363c75facc495904e81652fd9d59ce5bce3244
Ajay Gopinath <ajay.gopinath@outlook.com>
2013-06-21 13:38:06 -0700

    changing up the map + fixing soldiers not moving with viewport shift

--- src/org/cyanojay/rts/world/units/Soldier.java
+++ src/org/cyanojay/rts/world/units/Soldier.java
@@ -18,13 +18,12 @@ public class Soldier implements Drawable {
 	public final static float MOVE_SPEED = 4f;
 	public final static float MAX_STEER = 2f;
 	public final static float SIZE = 12f;
-	public final static float SLOWING_RAD = 2f*Terrain.IMG_HEIGHT;
+	public final static float SLOWING_RAD = 3f*Terrain.IMG_HEIGHT;
 	public final static float STOPPING_RAD = 0.5f*Terrain.IMG_HEIGHT;
 	
 	private Vector2f position;
 	private Vector2f velocity;
 	private Color color;
-	private Ellipse2D.Float body;
 	private long uid;
 	private UnitState state;
 	private Pathway currPath;
@@ -33,14 +32,14 @@ public class Soldier implements Drawable {
 	public Soldier(Vector2f pos, Color c) {
 		position = pos;
 		color = c;
-		body = new Ellipse2D.Float(position.x, position.y, SIZE, SIZE);
+		//body = new Ellipse2D.Float(position.x, position.y, SIZE, SIZE);
 		velocity = new Vector2f();
 		uid = GameUtil.getUID();
 	}
 	
 	public void draw(Graphics2D g, int xOff, int yOff) {
 		g.setColor(color);
-		g.fill(body);
+		g.fillOval((int)(position.x+xOff-SIZE/2), (int)(position.y+yOff-SIZE/2), (int)SIZE, (int)SIZE);
 		Vector2f velocityScale = Vmath.mult(velocity, 4f);
 		g.setColor(Color.GREEN);
 		g.drawLine((int)(xOff+position.x), (int)(yOff+position.y), (int)(xOff+position.x+velocity.x+velocityScale.x), (int)(yOff+position.y+velocity.y+velocityScale.y));
@@ -52,8 +51,6 @@ public class Soldier implements Drawable {
 
 	public void setPosition(Vector2f position) {
 		this.position = position;
-		body.x = position.x-SIZE/2;
-		body.y = position.y-SIZE/2;
 	}
 	
 	public Vector2f getVelocity() {
