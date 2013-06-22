81c32640576ade8d1aa3cd080e64472c97c830ae
Ajay Gopinath <ajay.gopinath@outlook.com>
2013-06-22 09:10:44 -0700

    fixing UnitState enum

--- src/org/cyanojay/rts/world/units/UnitState.java
+++ src/org/cyanojay/rts/world/units/UnitState.java
@@ -1,14 +1,5 @@
 package org.cyanojay.rts.world.units;
 
 public enum UnitState {
-	STANDING, MOVE_E, MOVE, ARRIVED;
-	enum WALKING {
-		
-	}
-	
-	private final String assetFolderName;
-	UnitState(String name) {
-		
-	}
-	String getAssetName()
+	STANDING, MOVING, ARRIVED;
 }
